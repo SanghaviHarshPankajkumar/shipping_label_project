848a4be887b08c7b30e5c1742769b2a3b385fb67
pritmanvar-aapkaCFO <pritmanvar1109@gmail.com>
2024-03-22 16:57:26 +0530

    completed object detection

---
+++ ObjectDetection/functions.py
@@ -0,0 +1,105 @@
+import torch
+import cv2
+import numpy as np
+
+def cropBlackBackground(img):
+    """Removes black background of labbel image and returns portion which contains label only.
+    It will need gray image.
+
+    Args:
+        img (ndarray): Numpy array representation of image
+
+    Returns:
+        img (ndarray): Numpy array representation of cropped image
+    """
+    try:
+        _, binary = cv2.threshold(img, 1, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        x, y, w, h = cv2.boundingRect(contours[0])
+        img = img[y:y+h, x:x+w]
+        
+        return img
+    except Exception as e:
+        print(e)
+        return None
+
+def enhanceImage(img, block_size: int = 19, constant: int = 5, adaptive_thresold_type = "GAUSSIAN", need_to_sharp: bool = True):
+    """Enhance image by appling adaptive thresolding and filter2D
+
+    Args:
+        img (ndarray): Numpy array representation of image
+        block_size (int, optional): Block size for adaptive thresolding. Defaults to 25.
+        constant (int, optional): Constant for adaptive thresolding. Defaults to 10.
+        adaptive_thresold_type (str, optional): "GAUSSIAN" or "MEAN. Defaults to "GAUSSIAN".
+        need_to_sharp (bool, optional): Defaults to True.
+
+    Returns:
+        img (ndarray): Numpy array representation of enhanced image
+    """
+    try:
+        if block_size < 2:
+            block_size = 2
+        block_size = block_size + 1 if block_size%2 == 0 else block_size
+        
+        final_img = img
+        if adaptive_thresold_type == "MEAN":
+            final_img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,block_size,constant)
+        else:
+            final_img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,block_size,constant)
+
+        if need_to_sharp:
+            kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+            final_img = cv2.filter2D(final_img, -1, kernel)
+            
+        return final_img
+    except Exception as e:
+        print(e)
+        return None
+
+def generateMask(res, original_img):
+    """_summary_
+
+    Args:
+        res: Resulf of yolo image segmentation for single mask
+        original_img (ndarray): Numpy array representation of original image
+
+    Returns:
+        tupple (ndarray, ndarray): (crop_img, mask)
+    """
+    try:
+        height,width,channel  = original_img.shape
+        masks = res.masks.data
+        boxes = res.boxes.data
+        
+        # extract classes
+        clss = boxes[:, 5]
+        # get indices of ress where class is 0 
+        label_indices = torch.where(clss == 0)
+        # use these indices to extract the relevant masks
+        label_masks = masks[label_indices]
+        # scale for visualizing ress
+        label_mask = torch.any(label_masks, dim=0).int() * 255
+
+        #final mask 
+        final_mask = label_mask.cpu().numpy()
+        height_mask,width_mask =final_mask.shape
+        fy = height/height_mask
+        fx = width/width_mask
+        final_mask = cv2.resize(final_mask,(0,0),fx =fx ,fy = fy,interpolation=cv2.INTER_NEAREST)
+
+        original_img =  original_img.astype(np.uint8)
+        final_mask = final_mask.astype(np.uint8)
+        
+        # Expand boundries
+        kernel = np.ones((40,40), np.uint8)
+        expanded_mask =  cv2.dilate(final_mask, kernel)
+
+        #crop_img
+        crop_img = cv2.bitwise_and(original_img,original_img,mask=expanded_mask)
+
+
+        return crop_img, expanded_mask
+
+    except Exception as e:
+        print(e)
+        return None, None
---
+++ ObjectDetection/object_detection.py
@@ -0,0 +1,64 @@
+import cv2
+from ultralytics import YOLO 
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+from functions import cropBlackBackground, enhanceImage, generateMask 
+
+path = {
+        'SEG_MODEL_PATH': str(os.getenv('SEG_MODEL_PATH')),
+        'DET_MODEL_PATH': str(os.getenv('DET_MODEL_PATH')),
+        'IMG_DIR_PATH': str(os.getenv('IMG_DIR_PATH')),
+        'INFERENCE_FOLDER': str(os.getenv('INFERENCE_FOLDER')),
+        }
+
+#import models
+seg_model = YOLO(path['SEG_MODEL_PATH'])
+det_model = YOLO(path['DET_MODEL_PATH'])
+
+CONF = 0.7  
+
+
+# do inference for detection and store croped images in folder
+for img in os.listdir(path['IMG_DIR_PATH']):
+    img_file = cv2.imread(os.path.join(path['IMG_DIR_PATH'],img),0)
+    cv2.imwrite(os.path.join(path['IMG_DIR_PATH'],img),img_file)
+    
+    det_model(os.path.join(path['IMG_DIR_PATH'],img),conf = CONF, save=True , save_crop=True , name=path['INFERENCE_FOLDER'],exist_ok = True)
+
+#do inference for image segmentation and store image in folder
+for img in os.listdir(path['IMG_DIR_PATH']):
+    img_file = cv2.imread(os.path.join(path['IMG_DIR_PATH'],img),0)
+    cv2.imwrite(os.path.join(path['IMG_DIR_PATH'],img),img_file)
+    
+    result = seg_model(os.path.join(path['IMG_DIR_PATH'],img),conf = CONF,save = True,name = path['INFERENCE_FOLDER'],exist_ok = True)
+    
+    original_img = cv2.imread(os.path.join(path['IMG_DIR_PATH'],img))
+    for res in result:
+        crop_img, mask = generateMask(res, original_img)
+        
+        image = None
+        if crop_img is not None:
+            # Convert to gray scale image
+            crop_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+            crop_img = cropBlackBackground(crop_img)
+            image = enhanceImage(crop_img)
+        
+        # save to file
+        if image is not None:
+            try: 
+                Path('runs').mkdir(parents=True, exist_ok=True)
+                Path(os.path.join('runs', 'segment')).mkdir(parents=True, exist_ok=True)
+                Path(os.path.join('runs', 'segment', 'inference_seg')).mkdir(parents=True, exist_ok=True)
+                Path(os.path.join('runs', 'segment', 'inference')).mkdir(parents=True, exist_ok=True)
+                Path(os.path.join('runs', 'segment', 'inference', 'crops_seg')).mkdir(parents=True, exist_ok=True)
+                Path(os.path.join('runs', 'segment', 'inference', 'enhanced')).mkdir(parents=True, exist_ok=True)
+            except OSError as error:  
+                print(error)
+                pass
+            
+            cv2.imwrite(os.path.join('runs', 'segment', 'inference_seg')+ img, mask)
+            cv2.imwrite(os.path.join('runs', 'segment', 'inference', 'crops_seg', img), crop_img )
+            cv2.imwrite(os.path.join('runs', 'segment', 'inference', 'enhanced', img), image )

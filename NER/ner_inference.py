6a9c757c2accd227fb530a990e5682d59cd92762
SanghaviHarshPankajkumar <harshpankajsanghai@gmail.com>
2024-04-02 13:55:57 +0530

    ner_update

--- NER/ner_inference.py
+++ NER/ner_inference.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
+import re
 
 path = {
         'NER_MODEL_PATH': str(os.getenv('NER_MODEL_PATH')),
@@ -31,4 +32,27 @@ def inference(sent):
         else:
             label_name[sorted_labels[0]['value']].append(label['text'])
 
+
+    #getting companie name
+    #1. get name directly from sentence
+    # company_name = get_company_name(sent.lower())
+    #2. track id from sentence
+
     return label_name
+
+def get_company_name(sent):
+    patterns = []
+    patterns.append({'company': "FeDex", 'pattern' : re.compile(r"\b{}\b".format("fedex")),"track_pattern":re.compile(r"\b{}\b".format("0003315500"))})
+    patterns.append({'company':'UPS', 'pattern':re.compile(r"\b{}\b".format("ups")),"track_pattern":re.compile(r"\b{}\b".format("1Z"))})
+    patterns.append({'company':'USPS', 'pattern':re.compile(r"\b{}\b".format("usps")),"track_pattern":re.compile(r"\b{}\b".format("42033155"))})
+    patterns.append({'company':'onTrack', 'pattern':re.compile(r"\b{}\b".format("ontrack")),"track_pattern":re.compile(r"\b{}\b".format("[0-9]{12}"))})
+    patterns.append({'company':'Lasership', 'pattern':re.compile(r"\b{}\b".format('1LS')),"track_pattern":re.compile(r"\b{}\b".format('1LS'))})
+    patterns.append({'company':'Amazon', 'pattern':re.compile(r"\b{}\b".format("DM16")),"track_pattern":re.compile(r"\b{}\b".format("TBA"))})
+
+   
+    for pattern in patterns:
+        if pattern['pattern'].search(sent) is not None:
+            return pattern['company']
+    return None
+
+

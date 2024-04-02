03b733cba04906455fde593f82824b50fab67cb3
SanghaviHarshPankajkumar <harshpankajsanghai@gmail.com>
2024-04-02 18:22:22 +0530

    changes in ner inference

--- NER/ner_inference.py
+++ NER/ner_inference.py
@@ -4,7 +4,7 @@ from flair.models import SequenceTagger
 import pathlib
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
-
+import json
 import os
 from dotenv import load_dotenv
 from pathlib import Path
@@ -24,18 +24,27 @@ def inference(sent):
     res = model.predict(sentence)
 
     res = sentence.to_dict()
-    label_name = {}
+    label_name = {'GCNUM':[],'TRACK-ID':[],'Company':"" }
     for label in res['entities']:
         sorted_labels = sorted(label['labels'], key=lambda k: k['confidence']) 
         if sorted_labels[0]['value'] not in label_name:
-            label_name[sorted_labels[0]['value']] = [label['text']]
+            label_name[sorted_labels[0]['value']].append([label['text']])
         else:
-            label_name[sorted_labels[0]['value']].append(label['text'])
+            label_name[sorted_labels[0]['value']].append([label['text']])
 
-
+    print(label_name)
+    if label_name['GCNUM'] is None:
+        pattern = re.compile(r"\b{}\b".format("[G,O,Q][C,O,Q][0-9]{5}"))
+        result = pattern.search(sent)
+        if result is not None:
+            label_name['GCNUM'].append(result.group(0))
+    
+    label_name['GCNUM'] = [json.dumps(label_name['GCNUM'])]
+    label_name['TRACK-ID'] = [json.dumps(label_name['TRACK-ID'])]
     #getting companie name
     #1. get name directly from sentence
-    # company_name = get_company_name(sent.lower())
+    label_name['Company'] = get_company_name(sent.lower())
+
     #2. track id from sentence
 
     return label_name
@@ -43,15 +52,19 @@ def inference(sent):
 def get_company_name(sent):
     patterns = []
     patterns.append({'company': "FeDex", 'pattern' : re.compile(r"\b{}\b".format("fedex")),"track_pattern":re.compile(r"\b{}\b".format("0003315500"))})
+    patterns.append({'company': "FeDex", 'pattern' : re.compile(r"\b{}\b".format("fedex")),"track_pattern":re.compile(r"\b{}\b".format("[0-9]{12}"))})
     patterns.append({'company':'UPS', 'pattern':re.compile(r"\b{}\b".format("ups")),"track_pattern":re.compile(r"\b{}\b".format("1Z"))})
     patterns.append({'company':'USPS', 'pattern':re.compile(r"\b{}\b".format("usps")),"track_pattern":re.compile(r"\b{}\b".format("42033155"))})
-    patterns.append({'company':'onTrack', 'pattern':re.compile(r"\b{}\b".format("ontrack")),"track_pattern":re.compile(r"\b{}\b".format("[0-9]{12}"))})
+    patterns.append({'company':'onTrack', 'pattern':re.compile(r"\b{}\b".format("ontrack")),"track_pattern":re.compile(r"\b{}\b".format("BG[0-9]{5}"))})
     patterns.append({'company':'Lasership', 'pattern':re.compile(r"\b{}\b".format('1LS')),"track_pattern":re.compile(r"\b{}\b".format('1LS'))})
-    patterns.append({'company':'Amazon', 'pattern':re.compile(r"\b{}\b".format("DM16")),"track_pattern":re.compile(r"\b{}\b".format("TBA"))})
+    patterns.append({'company':'Amazon', 'pattern':re.compile(r"\b{}\b".format("dmi6 | dm16")),"track_pattern":re.compile(r"\b{}\b".format("T[B,8]A"))})
 
-   
     for pattern in patterns:
         if pattern['pattern'].search(sent) is not None:
+            return pattern['company']
+    
+    for pattern in patterns:
+        if pattern['track_pattern'].search(sent) is not None:
             return pattern['company']
     return None
 
